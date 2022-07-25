5615debfd9de26dfadddd277aa0e709ec15b69b6
Keertiraj Malik <keertirajmalik@gmail.com>
2022-07-25 19:35:37 +0530

    rename variable extract the circular progress bar setup code

--- Goal App/ViewController.swift
+++ Goal App/ViewController.swift
@@ -12,36 +12,39 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var username: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
-    @IBOutlet weak var circularProgress1: CircularProgressView!
-    @IBOutlet weak var circle1Percentage: UILabel!
-    @IBOutlet weak var circularProgress2: CircularProgressView!
-    @IBOutlet weak var circle2Percentage: UILabel!
-    @IBOutlet weak var circularProgress3: CircularProgressView!
-    @IBOutlet weak var circle3Percentage: UILabel!
+    @IBOutlet weak var overDueProgress: CircularProgressView!
+    @IBOutlet weak var overDueProgressPercetange: UILabel!
+    @IBOutlet weak var completionRateProgress: CircularProgressView!
+    @IBOutlet weak var completionRateProgressPercentage: UILabel!
+    @IBOutlet weak var accuracyProgress: CircularProgressView!
+    @IBOutlet weak var accuracyProgressPercentage: UILabel!
     
     @IBOutlet weak var goalTasks: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let activityPercetage = Float(0.60)
-        circularProgress1.trackClr = UIColor.systemGray6
-        circularProgress1.progressClr = UIColor.red
-        circularProgress1.setProgressWithAnimation(duration: 0.75, value: activityPercetage)
-        circularProgress2.trackClr = UIColor.systemGray6
-        circularProgress2.progressClr = UIColor.orange
-        circularProgress2.setProgressWithAnimation(duration: 0.75, value: 0.95)
-        circularProgress3.trackClr = UIColor.systemGray6
-        circularProgress3.progressClr = UIColor.systemGreen
-        circularProgress3.setProgressWithAnimation(duration: 0.75, value: 0.95)
-        
-        circle1Percentage.text = "\(Int(activityPercetage * 100))"
-        circle2Percentage.text = "\(Int(activityPercetage * 100))"
-        circle3Percentage.text = "\(Int(activityPercetage * 100))"
-        
+        progressCircleSetup()
         goalTasks.register(TableViewCell.nib(), forCellReuseIdentifier: TableViewCell.identifier)
         goalTasks.dataSource = self
         goalTasks.delegate = self
+    }
+    
+    private func progressCircleSetup(){
+        let activityPercetage = Float(0.60)
+        overDueProgress.trackClr = UIColor.systemGray6
+        overDueProgress.progressClr = UIColor.red
+        overDueProgress.setProgressWithAnimation(duration: 0.75, value: activityPercetage)
+        completionRateProgress.trackClr = UIColor.systemGray6
+        completionRateProgress.progressClr = UIColor.orange
+        completionRateProgress.setProgressWithAnimation(duration: 0.75, value: 0.95)
+        accuracyProgress.trackClr = UIColor.systemGray6
+        accuracyProgress.progressClr = UIColor.systemGreen
+        accuracyProgress.setProgressWithAnimation(duration: 0.75, value: 0.95)
+        
+        overDueProgressPercetange.text = "\(Int(activityPercetage * 100))"
+        completionRateProgressPercentage.text = "\(Int(activityPercetage * 100))"
+        accuracyProgressPercentage.text = "\(Int(activityPercetage * 100))"
     }
 }
 
