24dd84751c1466ab9f4adc7484828ebe767820c0
Keertiraj Malik <keertirajmalik@gmail.com>
2022-07-26 16:22:35 +0530

    Show new screen when user click on the see all button

---
+++ Goal App/View/PersonalGoalsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PersonalGoalsVCViewController.swift
+//  Goal App
+//
+//  Created by Keertiraj Laxman Malik on 26/07/22.
+//
+
+import UIKit
+
+class PersonalGoalsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
