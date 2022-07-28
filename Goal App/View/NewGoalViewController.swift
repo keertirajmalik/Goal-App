a4449bfef02a3296b36c159db822bd8db02141d6
Keertiraj Malik <keertirajmalik@gmail.com>
2022-07-28 17:44:25 +0530

    Adding new goal screen

---
+++ Goal App/View/NewGoalViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewGoalViewController.swift
+//  Goal App
+//
+//  Created by Keertiraj Laxman Malik on 28/07/22.
+//
+
+import UIKit
+
+class NewGoalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
