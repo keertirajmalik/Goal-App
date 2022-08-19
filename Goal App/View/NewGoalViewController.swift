26c083f42f531519bfdefe07f4423a1ab36e42be
Keertiraj Malik <keertirajmalik@gmail.com>
2022-08-19 22:36:38 +0530

    Gave curved corners to creatDateView and dueDateView

--- Goal App/View/NewGoalViewController.swift
+++ Goal App/View/NewGoalViewController.swift
@@ -9,13 +9,20 @@ import UIKit
 
 class NewGoalViewController: UIViewController {
 
+    @IBOutlet weak var createdDateView: UIView!
+    @IBOutlet weak var dueDateView: UIView!
     @IBOutlet weak var goalNameTextField: UITextField!
     @IBOutlet weak var goalCreatedDate: UIDatePicker!
     @IBOutlet weak var goalDueDate: UIDatePicker!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         goalNameTextField.becomeFirstResponder()
         goalNameTextField.delegate = self
+        createdDateView.layer.cornerRadius = 5
+        createdDateView.layer.masksToBounds = true
+        dueDateView.layer.cornerRadius = 5
+        dueDateView.layer.masksToBounds = true
     }
     
     @IBAction func goalSaveButtonClicked(_ sender: UIButton) {
