775a704cb54e7abb2341ff8047bf7ff1fc4fde94
Keertiraj Malik <keertirajmalik@gmail.com>
2022-08-19 22:26:23 +0530

    Created Screen related to adding new personal goals

--- Goal App/View/NewGoalViewController.swift
+++ Goal App/View/NewGoalViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class NewGoalViewController: UIViewController {
 
+    @IBOutlet weak var goalNameTextField: UITextField!
+    @IBOutlet weak var goalCreatedDate: UIDatePicker!
+    @IBOutlet weak var goalDueDate: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
+        goalNameTextField.becomeFirstResponder()
+        goalNameTextField.delegate = self
+    }
+    
+    @IBAction func goalSaveButtonClicked(_ sender: UIButton) {
+        goalNameTextField.resignFirstResponder()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension NewGoalViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        textField.resignFirstResponder()
     }
 }
