0ff3c2aba446eece58ef4d7e2e0c2845b0b421af
Keertiraj Malik <keertirajmalik@gmail.com>
2022-09-10 23:48:34 +0530

    Created New VC for login screen separated Home screen from main storyboard

---
+++ Goal App/Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Goal App
+//
+//  Created by Keertiraj Laxman Malik on 10/09/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .systemBlue
+    }
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
+}
