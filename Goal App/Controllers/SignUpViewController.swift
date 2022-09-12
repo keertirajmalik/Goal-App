5229351e5fcb2d36ad49b1c7233558b0a2b922ef
Keertiraj Malik <keertirajmalik@gmail.com>
2022-09-12 23:35:26 +0530

    created sign up screen

---
+++ Goal App/Controllers/SignUpViewController.swift
@@ -0,0 +1,113 @@
+//
+//  LoginViewController.swift
+//  Goal App
+//
+//  Created by Keertiraj Laxman Malik on 10/09/22.
+//
+
+import FirebaseAuth
+import UIKit
+
+class SignUpViewController: UIViewController {
+    var signUpHeaderlabel: UILabel!
+    var userNameTextField: UITextField!
+    var passwordTextField: UITextField!
+    var emailTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userNameTextField.becomeFirstResponder()
+    }
+}
+
+extension SignUpViewController {
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+
+        signUpHeaderlabel = UILabel()
+        signUpHeaderlabel.translatesAutoresizingMaskIntoConstraints = false
+        signUpHeaderlabel.font = .systemFont(ofSize: 40, weight: .semibold)
+        signUpHeaderlabel.text = "Let's get started!"
+        view.addSubview(signUpHeaderlabel)
+
+        userNameTextField = UITextField()
+        userNameTextField.translatesAutoresizingMaskIntoConstraints = false
+        userNameTextField.placeholder = "User name"
+        userNameTextField.font = UIFont.systemFont(ofSize: 18)
+        userNameTextField.backgroundColor = .systemGray5
+        userNameTextField.layer.cornerRadius = 5
+        userNameTextField.setLeftPaddingPoints(10)
+        userNameTextField.setRightPaddingPoints(10)
+        view.addSubview(userNameTextField)
+
+        emailTextField = UITextField()
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.placeholder = "Email"
+        emailTextField.font = UIFont.systemFont(ofSize: 18)
+        emailTextField.backgroundColor = .systemGray5
+        emailTextField.layer.cornerRadius = 5
+        emailTextField.setLeftPaddingPoints(10)
+        emailTextField.setRightPaddingPoints(10)
+        view.addSubview(emailTextField)
+
+        passwordTextField = UITextField()
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.placeholder = "Password"
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.font = UIFont.systemFont(ofSize: 18)
+        passwordTextField.backgroundColor = .systemGray5
+        passwordTextField.layer.cornerRadius = 5
+        passwordTextField.setLeftPaddingPoints(10)
+        passwordTextField.setRightPaddingPoints(10)
+        view.addSubview(passwordTextField)
+
+        let signUpButton = UIButton(type: .system)
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        signUpButton.backgroundColor = .black
+        signUpButton.tintColor = .white
+        signUpButton.setTitle("Sign Up", for: .normal)
+        signUpButton.layer.cornerRadius = 5
+        view.addSubview(signUpButton)
+
+        let loginButton = UIButton(type: .system)
+        loginButton.backgroundColor = .white
+        loginButton.tintColor = .black
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.layer.cornerRadius = 5
+        view.addSubview(loginButton)
+
+        NSLayoutConstraint.activate([
+            signUpHeaderlabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            signUpHeaderlabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+
+            userNameTextField.topAnchor.constraint(equalTo: signUpHeaderlabel.bottomAnchor, constant: 20),
+            userNameTextField.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            userNameTextField.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            userNameTextField.heightAnchor.constraint(equalToConstant: 52),
+
+            emailTextField.topAnchor.constraint(equalTo: userNameTextField.bottomAnchor, constant: 20),
+            emailTextField.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            emailTextField.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            emailTextField.heightAnchor.constraint(equalToConstant: 52),
+
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
+            passwordTextField.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            passwordTextField.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 52),
+
+            signUpButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 30),
+            signUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signUpButton.heightAnchor.constraint(equalToConstant: 44),
+            signUpButton.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            signUpButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+
+            loginButton.topAnchor.constraint(equalTo: signUpButton.bottomAnchor, constant: 30),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.heightAnchor.constraint(equalToConstant: 44),
+            loginButton.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            loginButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+        ])
+    }
+}
